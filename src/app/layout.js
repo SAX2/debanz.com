7aab7fdc4a836cd1666f3c57bed2c7ae1e321faa
Santino Degra <santinodegra73@gmail.com>
2023-09-14 17:05:06 -0300

    remove getStaticProps from layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,8 @@
 import { CartContextProvider } from './context/CartContext';
 import BagContextProvider from './context/BagContext';
 import UserContextProvider from './context/UserContext';
-import './globals.css';
-import { storefront } from './utils';
-import { shopifyApi } from '@shopify/shopify-api';
 import { Inter } from 'next/font/google'
+import './globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,56 +13,6 @@ export const metadata = {
   author: "Debanz",
   viewport: "width=device-width, initial-scale=1.0",
   robots: "index, follow"
-}
-
-export const getStaticProps = async () => {
-  const gql = String.raw;
-  const { data } = await storefront(gql`
-    mutation CreateCart {
-      cartCreate {
-        cart {
-          id
-          createdAt
-          updatedAt
-          lines(first: 10) {
-            edges {
-              node {
-                id
-                merchandise {
-                  ... on ProductVariant {
-                    id
-                  }
-                }
-              }
-            }
-          }
-          # The estimated total cost of all merchandise that the customer will pay at checkout.
-          cost {
-            totalAmount {
-              amount
-              currencyCode
-            }
-            # The estimated amount, before taxes and discounts, for the customer to pay at checkout.
-            subtotalAmount {
-              amount
-              currencyCode
-            }
-            # The estimated tax amount for the customer to pay at checkout.
-            totalTaxAmount {
-              amount
-              currencyCode
-            }
-            # The estimated duty amount for the customer to pay at checkout.
-            totalDutyAmount {
-              amount
-              currencyCode
-            }
-          }
-        }
-      }
-    }
-  `);
-  return { props: { cartId: data.cart.id } }
 }
 
 export default function RootLayout({ children }) {
