32cb53b87a34e72bd8128cc8198915339ebd0e09
Santino Degra <santinodegra73@gmail.com>
2023-10-19 18:20:52 -0300

    EnviromentVariables

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
     SHIPNOWTOKEN: "uBN7-C25ho93igeBIZoEQruW4PYsRvxqgTpahiy_-uak_z-G0A",
     NEXT_PUBLIC_GOOGLE_ANALYTICS: "G-DKXNCWTL91",
     WEB_PASS: "CollapseByDebanz",
-    SHOPIFY_ACCESS_TOKEN: "422757de6c23015e81ceec4d3fa45aaa",
-    SHOPIFY_PULIC_API_URL: "https://136bf8-2.myshopify.com/api/2023-07/graphql.json"
+    SHOPIFY_ACCESS_TOKEN: "e36b41dfa468feba15b6c40f57cdf663",
+    SHOPIFY_PULIC_API_URL: "https://d4eea9.myshopify.com/api/2023-10/graphql.json"
   },
   experimental: {
     serverActions: true,
