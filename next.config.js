e95222e959e6cd2ca94256846eb757f4d2ea78b4
Santino Degra <91171834+SAX2@users.noreply.github.com>
2023-12-02 16:28:12 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
     domains: ["localhost", "debanz.com", "api.debanz.com", "cdn.shopify.com"],
   },
   env: {
-    SHIPNOWTOKEN: "uBN7-C25ho93igeBIZoEQruW4PYsRvxqgTpahiy_-uak_z-G0A",
-    NEXT_PUBLIC_GOOGLE_ANALYTICS: "G-DKXNCWTL91",
-    WEB_PASS: "CollapseByDebanz",
-    SHOPIFY_ACCESS_TOKEN: "e36b41dfa468feba15b6c40f57cdf663",
-    SHOPIFY_PULIC_API_URL: "https://d4eea9.myshopify.com/api/2023-10/graphql.json"
+    SHIPNOWTOKEN: null,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: null,
+    WEB_PASS: null,
+    SHOPIFY_ACCESS_TOKEN: null,
+    SHOPIFY_PULIC_API_URL: null
   },
   experimental: {
     serverActions: true,
